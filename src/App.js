28f0e6152266dafda54d9e8cce7d76083b13fd5a
Benja <birivera@uc.cl>
2021-05-11 19:17:05 -0400

    Editado y pipelineado

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Editado :)
+          Edit <code>src/App.js</code> and save to reload. Editado y pipelineado :)
         </p>
         <a
           className="App-link"
