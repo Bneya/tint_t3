b8d4289abf6888cc49c38fc2adda85f7657361bf
Benja <birivera@uc.cl>
2021-05-13 10:03:02 -0400

    Inicial commit with socket.io installed

---
+++ src/screens/mainmap.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import logo from '../assets/logo.svg';
+
+export default function MainMap() {
+  // javascript code
+  return (
+    // html-ish
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Desde el nuevo componente
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
