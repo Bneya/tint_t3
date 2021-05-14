84660096330ee4f65d17bf5e96631a3bc82f2e1b
Benja <birivera@uc.cl>
2021-05-14 16:38:00 -0400

    Added chat bonito y nicknames

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,32 +1,36 @@
 import { socket } from '../config/constants';
 import React, { useState, useEffect, createRef } from "react";
 
+import 'react-chat-elements/dist/main.css';
+import { MessageBox } from 'react-chat-elements';
+
 
 export default function Chat() {
   // js code
   // const socket = io(API_URL, { path: '/flights' })
 
 
-  const [message, setMessage] = useState([]);  // Hook
+  const [messages, setMessage] = useState([]);  // Hook
   let chatInput = createRef();
+  let nickname = createRef();
 
   useEffect(() => {
     socket.on("CHAT", data => {
       console.log('data', data);
-      setMessage(data);
+      setMessage(oldArray => [...oldArray, data]);
     });
     // Desconectar websocket
     return () => socket.disconnect();
   }, []);
 
   // Enviar mensaje
-  const fetchRequest = (e) => {
+  const sendMsg = (e) => {
        // Api request here
        console.log('llamando a la función con un botón. currText:', chatInput.current.value);
 
        // Enviar mensaje al socket
        const msgToSend = {
-         name: 'juanitoharcodeado',
+         name: nickname.current.value || 'Piloto desconocido',
          message: chatInput.current.value,
        }
        socket.emit('CHAT', msgToSend);
@@ -37,9 +41,23 @@ export default function Chat() {
 
   return(
     <div>
-      <div>el mensaje es: {message.name}, {message.date}, {message.message}</div>
+      {messages.map((msg) => {
+        const dateraw = new Date(msg.date);
+        const date = `${dateraw.getHours()}:${dateraw.getMinutes()}`;
+        return(
+
+          <MessageBox
+            text={msg.message}
+            title={msg.name}
+            dateString={date}
+          />
+        )
+      })}
+
+      Nickame
+      <input ref={nickname} defaultValue="Piloto desconocido"/>
       <input ref={chatInput} />
-      <input type="button" onClick={fetchRequest} value="Enviar"/>
+      <input type="button" onClick={sendMsg} value="Enviar"/>
     </div>
   )
 }
